99684e9fd2a0346ee57d276a4c6692ccf3d3dc18
victorious01 <victorious.borbon@gmail.com>
2024-10-25 20:10:14 +0800

    own_laravel_proj_practice

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body{
+            min-height: 800px;
+            border: 8px solid black;
+            display: flex;
+            flex-direction:row;
+            justify-content: center;
+            align-items: center;
+            gap: 20px;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+            align-content: flex-start;
+        }
+        .box {
+            height: 200px;
+            width: 200px;
+            background-color: #0071FF;
+            font-size: 40pt;
+        }
+        
+    </style>
+</head>
+<body>
+    <div class="box">1</div>
+    <input type="name" name="name" placeholder="name">
+    <div class="box">2</div>
+    <div class="box">3</div>
+    <div class="box">4</div>
+    <div class="box">5</div>    
+    <div class="box">6</div>
+    <div class="box">7</div>
+    <div class="box">8</div>
+    <div class="box">9</div> 
+</body>
+</html>
